74c9944228511f13fd0eb06a854862ac23e25c9b
GoylikDmitriy <goylik.dmitriy@gmail.com>
2026-03-14 19:20:22 +0300

    integration tests

--- src/main/java/com/goylik/user_service/config/RedisCacheConfig.java
+++ src/main/java/com/goylik/user_service/config/RedisCacheConfig.java
@@ -1,27 +1,34 @@
 package com.goylik.user_service.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import tools.jackson.databind.ObjectMapper;
 
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@ConditionalOnProperty(
+        name = "spring.cache.type",
+        havingValue = "redis",
+        matchIfMissing = true
+)
 public class RedisCacheConfig {
     @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory, ObjectMapper objectMapper) {
         RedisCacheConfiguration defaultConfig =
                 RedisCacheConfiguration.defaultCacheConfig()
                         .entryTtl(Duration.ofMinutes(10))
                         .serializeValuesWith(
                                 RedisSerializationContext.SerializationPair
-                                        .fromSerializer(RedisSerializer.json())
+                                        .fromSerializer(new GenericJacksonJsonRedisSerializer(objectMapper))
                         )
                         .computePrefixWith(cacheName -> "user-service::" + cacheName + "::")
                         .disableCachingNullValues();
