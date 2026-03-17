3f2bf9c8026f65e980f10c80b224ee6b7aeaa968
GoylikDmitriy <goylik.dmitriy@gmail.com>
2026-03-17 22:35:25 +0300

    fix: added scheduler config

---
+++ src/main/java/com/goylik/user_service/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.goylik.user_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
